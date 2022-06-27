d0eb2a9dfa37d0668bd64fbcead307420460f395
HP <hasanta.vision@gmail.com>
2022-06-27 15:39:07 +0700

    Adding get_object and list_or_404

--- hvision/core/views.py
+++ hvision/core/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.http import HttpResponse
+
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return HttpResponse(
-        "<h1>test</h1>"
-    )
+    value1 = 0
+    value2 = 0
+    context = {'parameter1': value1, 'parameter2': value2}
+    return render(request, 'home/index.html', context)
