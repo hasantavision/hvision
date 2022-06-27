8af3da2694c9ae390c27c6c3d2be0af278c23442
HP <hasanta.vision@gmail.com>
2022-06-27 11:15:02 +0700

    Initial Commit

---
+++ hvision/core/models.py
@@ -0,0 +1,10 @@
+from djongo import models
+
+# Create your models here.
+class Article(models.Model):
+    article_title = models.CharField(max_length=200)
+    article_slug = models.CharField(max_length=200, default='')
+    article_pub_date = models.DateField('published')
+    article_description = models.TextField(max_length=500, default='')
+    article_body = models.TextField(max_length=10000, default='')
+    article_author = models.TextField()
