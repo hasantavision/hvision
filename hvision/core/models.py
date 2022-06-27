11ada1c1c613690189f064e6b4b4c381a9eab046
HP <hasanta.vision@gmail.com>
2022-06-27 13:24:17 +0700

    change article model DateField to DateTimeField

--- hvision/core/models.py
+++ hvision/core/models.py
@@ -4,7 +4,7 @@ from djongo import models
 class Article(models.Model):
     article_title = models.CharField(max_length=200)
     article_slug = models.CharField(max_length=200, default='')
-    article_pub_date = models.DateField('published')
+    article_pub_date = models.DateTimeField('published')
     article_description = models.TextField(max_length=500, default='')
     article_body = models.TextField(max_length=10000, default='')
     article_author = models.TextField()
